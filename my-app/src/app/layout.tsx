55d7a6dad2fc4f341c2a694f0831f1eefba6da27
dessygil <desmondgilmour@gmail.com>
2023-04-22 08:30:29 -0400

    added the benzene preloader to my site

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,23 +1,26 @@
 import './globals.css'
+
+// Dependency for accessibility testing
 if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
   const ReactDOM = require('react-dom');
   const axe = require('@axe-core/react');
   axe(React, ReactDOM, 1000);
 }
 
+// Page metadata
 export const metadata = {
   title: 'Desmond Gilmour',
   description: 'Desmond Gilmour\'s personal website',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+
+export default function RootLayout({ children }) {
+  
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,7 +1,25 @@
+"use client"; 
+
+import React, { useState } from 'react';
+import Preloader from './preloader/preloader';
+
 
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+
+  // Setting up preloader animation
+  React.useEffect(() =>{
+    setTimeout(() => setLoading(true), 7000);
+   })
+
   return (
-    
+    <>
+      {!loading ? (
+        <Preloader />
+      ) : (
+        <p>I am now here</p >
+      )}
+    </>
   )
 }
